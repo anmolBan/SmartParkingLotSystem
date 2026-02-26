d8ba62898a9d207adc00de3f318cb60905d3af10
Anmol Bansal <bansal.anmol98@gmail.com>
2026-02-27 03:25:21 +0530

    Update

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -4,6 +4,8 @@ import { adminSignInSchema } from '../../validators/userSchemas.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { User } from '../../generated/prisma/client.js';
+import { createParkingLotSchema } from '../../validators/parkingSchema.js';
+import { authMiddleware } from '../../middlewares/auth.js';
 
 dotenv.config();
 
@@ -32,7 +34,7 @@ router.post('/signin', async (req: Request, res: Response) => {
         return res.status(401).json({ error: 'Invalid email or password' });
     }
 
-    const token = jwt.sign({ userId: admin.id, type: admin.type, email: admin.email }, process.env.JWT_SECRET!, { expiresIn: '48h' });
+    const token = jwt.sign({ userId: admin.id, type: admin.type, email: admin.email }, process.env.JWT_SECRET!, { expiresIn: '12h' });
 
     res.json({
         message: 'Sign-in successful',
@@ -40,8 +42,33 @@ router.post('/signin', async (req: Request, res: Response) => {
     });
 });
 
-router.post('/createParkingLot', async (req: Request, res: Response) => {
-    
+router.post('/createParkingLot', authMiddleware, async (req: Request, res: Response) => {
+    const body = req.body;
+
+    const parsedBody = createParkingLotSchema.safeParse(body);
+
+    if (!parsedBody.success) {
+        return res.status(400).json({ error: parsedBody.error.issues.map(issue => issue.message) });
+    }
+
+    const { name, location, floors, spotsPerFloor } = parsedBody.data;
+
+    try {
+        const newParkingLot = await prisma.parkingLot.create({
+            data: {
+                name,
+                location,
+                floors,
+                spotsPerFloor
+            }
+        });
+        res.status(201).json({ 
+            message: 'Parking lot created successfully',
+            newParkingLot
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to create parking lot' });
+    }
 });
 
 export default router;
--- src/validators/parkingSchema.ts
+++ src/validators/parkingSchema.ts
@@ -3,6 +3,6 @@ import zod from "zod";
 export const createParkingLotSchema = zod.object({
     name: zod.string().min(1).max(255),
     location: zod.string().min(1).max(255),
-    capacity: zod.number().int().positive(),
     floors: zod.number().int().positive(),
+    spotsPerFloor: zod.number().int().positive(),
 });
